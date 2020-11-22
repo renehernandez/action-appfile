0690fec9b28141316ed46c4a40174ea60a099eae
renehernandez <renehr9102@protonmail.com>
2020-11-22 07:43:19 -0500

    Update package

--- dist/index.js
+++ dist/index.js
@@ -1761,6 +1761,7 @@ async function run() {
         if (version.charAt(0) === 'v') {
             version = version.substr(1);
         }
+        core.info(`>>> Version to use: ${version}`);
 
         var path = tc.find("appfile", version);
         if (!path) {
