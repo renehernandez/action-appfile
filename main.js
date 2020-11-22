0be81dfbb65170872376c46d83a8a82c69c42c1d
renehernandez <renehr9102@protonmail.com>
2020-11-22 08:04:05 -0500

    Return directory path instead of filepath

--- main.js
+++ main.js
@@ -2,22 +2,32 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tc = require('@actions/tool-cache');
 const { Octokit } = require("@octokit/rest");
+import * as path from 'path'
 
 const baseDownloadURL = "https://github.com/renehernandez/appfile/releases/download"
 const fallbackVersion = "0.0.1"
 const octokit = new Octokit();
 
+/**
+ * Gets RUNNER_TEMP
+ */
+function _getTempDirectory() {
+    const tempDirectory = process.env['RUNNER_TEMP'] || ''
+    ok(tempDirectory, 'Expected RUNNER_TEMP to be defined')
+    return tempDirectory
+  }
+
 async function downloadAppfile(version) {
     if (process.platform === 'win32') {
         const appfileDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/appfile_windows_amd64.exe`);
-        return appfileDownload;
+        return path.dirname(appfileDownload);
     }
     if (process.platform === 'darwin') {
         const appfileDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/appfile_darwin_amd64`);
-        return appfileDownload;
+        return path.dirname(appfileDownload);
     }
     const appfileDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/appfile_linux_amd64`);
-    return appfileDownload;
+    return path.dirname(appfileDownload);
 }
 
 async function run() {
